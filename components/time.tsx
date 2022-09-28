1e3386413ce36e1197bfe390a4b4332fd8b2a338
Matthew Stanciu <mattbstanciu@gmail.com>
2022-09-28 16:31:17 -0400

    Add title thing

--- components/time.tsx
+++ components/time.tsx
@@ -17,16 +17,34 @@ const Time = () => {
   }, [])
 
   return (
-    <div className="flex flex-col items-center justify-center w-full tabular-nums">
-      <h1
-        className={`${
-          dateObj.getHours() > 22 || dateObj.getHours() === 0
-            ? 'text-[7vw]'
-            : 'text-[8vw]'
-        } font-bold underline underline-offset-[12px] text-amber-300 decoration-white decoration-dotted decoration-8`}
-      >
-        {time}
-      </h1>
+    <div className="grid grid-rows-3 items-center justify-center w-full tabular-nums">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold">
+          <img src="/ph-logo-cropped.png" className="inline-block h-[3.5rem]" />{' '}
+          Hack Night 🌚
+        </h1>
+        <div className="flex flex-row gap-x-2">
+          <h1 className="text-gray-300 text-3xl font-bold underline underline-offset-8">
+            0.3
+          </h1>
+          <div className="flex flex-col justify-center">
+            <div className="bg-amber-400 text-black rounded px-1">
+              <p className="text-sm font-bold">beta</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <h1
+          className={`${
+            dateObj.getHours() > 22 || dateObj.getHours() === 0
+              ? 'text-[7vw]'
+              : 'text-[8vw]'
+          } font-bold underline underline-offset-[12px] text-amber-300 decoration-white decoration-dotted decoration-8`}
+        >
+          {time}
+        </h1>
+      </div>
     </div>
   )
 }
