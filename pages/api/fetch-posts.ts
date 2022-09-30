391664e0b83f2b988979ae45e58648c63779a2c8
Matthew Stanciu <mattbstanciu@gmail.com>
2022-09-30 19:57:07 -0400

    Fix it in a better way unlike the stupid way

--- pages/api/fetch-posts.ts
+++ pages/api/fetch-posts.ts
@@ -1,34 +1,7 @@
-import Airtable, { Attachment } from 'airtable'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { Post } from '../../types/posts'
-
-Airtable.configure({
-  endpointUrl: 'https://api.airtable.com',
-  apiKey: process.env.AIRTABLE_API_KEY
-})
-const base = Airtable.base('appCdXVAcRjUahF5M')
+import { fetchPosts } from '../../lib/fetch-posts'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  base('Scraps')
-    .select({
-      maxRecords: 3,
-      view: 'Grid view',
-      sort: [{ field: 'Created time', direction: 'desc' }]
-    })
-    .firstPage(async (err, records) => {
-      if (err) console.log('error', err)
-      if (!records) {
-        return res.status(404).send('no records found')
-      }
-
-      const posts: Post[] = records.map((record) => ({
-        id: record.id,
-        createdTime: record.get('Created time') as string,
-        description: record.get('Description') as string,
-        attachments: record.get('Attachments') as Attachment[],
-        username: record.get('Username (from User)') as string,
-        avatar: record.get('Avatar (from User)') as Attachment[]
-      }))
-      return res.json(posts)
-    })
+  const posts = await fetchPosts()
+  res.json(posts)
 }
