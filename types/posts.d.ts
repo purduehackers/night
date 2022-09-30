cc9e8a902dbb9b4dcd55a2c19c4ac3ea35a553a3
Matthew Stanciu <mattbstanciu@gmail.com>
2022-09-30 17:31:08 -0400

    remove unused type

--- types/posts.d.ts
+++ types/posts.d.ts
@@ -1,15 +1,4 @@
 import { Attachment } from 'airtable'
-
-interface AirtableAttachment {
-  id: string
-  size: number
-  width: number
-  height: number
-  url: string
-  type: string
-  filename: string
-}
-
 interface Post {
   createdTime: string
   description: string
