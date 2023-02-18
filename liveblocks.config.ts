8113a2f6d84d50b6a1ac828e54ca98e6a6fd201b
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-17 20:17:14 -0500

    Fix Liveblocks?

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,7 +2,7 @@ import { createClient } from '@liveblocks/client'
 import { createRoomContext } from '@liveblocks/react'
 
 const client = createClient({
-  publicApiKey: process.env.LIVEBLOCKS_API_KEY ?? ''
+  publicApiKey: 'pk_prod_9TiyTWR-55DbLFR_CwB8F70P61daBNJI5SuPgh92zV_2xV47lMeZKnqGfZyeBW-C'
 })
 
 export type Presence = {}
