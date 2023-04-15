ea3f1ce3ed960e59f256cb696757c5cd4cebc6dc
Matthew Stanciu <mattbstanciu@gmail.com>
2023-04-14 20:07:04 -0400

    2.6 for real

--- components/time.tsx
+++ components/time.tsx
@@ -30,7 +30,7 @@ const Time = () => {
           HACK NIGHT
         </h1>
         <div className="flex flex-row gap-x-2">
-          <h1 className="text-3xl font-bold text-gray-300">2.5</h1>
+          <h1 className="text-3xl font-bold text-gray-300">2.6</h1>
           <div className="flex flex-row items-center gap-1">
             <div className="px-1 text-black rounded bg-gradient-to-r from-gray-400 to-gray-500">
               <p className="text-sm font-bold">now reflective!</p>
