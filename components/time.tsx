ea6035c91916742cd3ccbc46a641a641ac79dff5
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-22 19:57:33 -0400

    3.2

--- components/time.tsx
+++ components/time.tsx
@@ -30,12 +30,10 @@ const Time = () => {
           HACK NIGHT
         </h1>
         <div className="flex flex-row gap-x-2">
-          <h1 className="text-3xl font-bold text-gray-300">3.1</h1>
+          <h1 className="text-3xl font-bold text-gray-300">3.2</h1>
           <div className="flex flex-row items-center gap-1">
-            <div className="px-1 text-white rounded bg-orange-500">
-              <p className="text-sm font-bold">
-                now fully rotated around the Sun!
-              </p>
+            <div className="px-1 text-black rounded bg-green-400">
+              <p className="text-sm font-bold">now more puzzling than ever!</p>
             </div>
           </div>
         </div>
